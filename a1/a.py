887706ddf0b054de1ca52e8c986897d981d67e0a
moriyatakashi <36041907+moriyatakashi@users.noreply.github.com>
2025-10-17 10:12:33 +0900

    a

--- a1/a.py
+++
@@ -1,4 +0,0 @@
-from z import*
-pa=ab("x.txt")
-pb=ac(pa,1)
-ad(pb)
--- a1/z.py
+++
@@ -1,6 +0,0 @@
-def ab(pa):
-    return open("x.txt").read()
-def ac(pa,pb):
-    return pa.split("\n")[pb][10:].split(" ")[0]
-def ad(pa):
-    print(" ".join(f'{pb:02x}'for pb in bytes.fromhex(pa)))
