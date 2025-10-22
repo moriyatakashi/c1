5401161942f5dcc56c36125d94628d296eeeb297
takashi <takashi.moriya@gmail.com>
2025-10-23 06:26:52 +0900

    a

---
+++ a4/a.py
@@ -0,0 +1,12 @@
+AA=0
+def aa(pa):
+    for pb in range(len(pa)):
+        if pa[pb]==" ":break
+    return pb
+def ab(pa,pb):
+    0
+pa=open("a.txt").read().split("\n")[0]
+print(pa)
+pb=[0]
+pb+=[aa(pa)]
+print(pb)
