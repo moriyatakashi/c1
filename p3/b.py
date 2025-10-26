2a52683b46d3abfdf6ca9c3281c52e99653004a7
takashi <takashi.moriya@gmail.com>
2025-10-26 10:24:46 +0900

    a

--- p3/b.py
+++ p3/b.py
@@ -1,8 +1,8 @@
 import pygame as a
 a.init()
-scre=a.display.set_mode((640,480))
+scrn=a.display.set_mode((640,480))
 font=a.font.Font(None,36)
 txsf=font.render("OK",1,[255]*3)
-scre.blit(txsf,[0]*2)
+scrn.blit(txsf,[0]*2)
 a.display.flip()
 a.time.wait(3000)
