f28d55614826f84b6a98458fab8dc917e84899f6
takashi <takashi.moriya@gmail.com>
2025-10-27 21:44:01 +0900

    a

--- d1/main.js
+++ d1/main.js
@@ -1,10 +1,5 @@
-const { app, BrowserWindow } = require('electron');
-
-app.whenReady().then(() => {
-  const win = new BrowserWindow({
-    width: 640,
-    height: 480,
-    resizable: false,
-  });
-  win.loadFile('index.html');
+const{app,BrowserWindow}=require('electron');
+app.whenReady().then(()=>{
+const win=new BrowserWindow({width:640,height:480});
+win.loadFile('index.html');
 });
