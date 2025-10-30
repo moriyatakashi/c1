f6c45a405abc9ee99d8c2f954d877936cf291bf1
moriyatakashi <36041907+moriyatakashi@users.noreply.github.com>
2025-10-30 10:58:45 +0900

    a

--- p2/b1.py
+++
@@ -1,7 +0,0 @@
-def aa(pa,pb,pc):
-    import tkinter as a
-    b=a.Tk()
-    b.geometry(str(pa)+"x"+str(pb))
-    a.Label(b,text=pc).place(x=0,y=0)
-    b.mainloop()
-aa(99,99,"OK")
--- p2/b4.py
+++
@@ -1,5 +0,0 @@
-import sys
-import json
-pa=open(sys.argv[1]).read()
-pb=json.loads(pa)
-print(pb)
