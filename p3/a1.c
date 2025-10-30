c74af6aa5e9fc1373761c64415e873400a4b5403
takashi <takashi.moriya@gmail.com>
2025-10-30 22:21:00 +0900

    a

--- p3/a1.c
+++
@@ -1,6 +0,0 @@
-#include <windows.h>
-LRESULT CALLBACK W(HWND h,UINT m,WPARAM w,LPARAM l){return m==2?(PostQuitMessage(0),0):DefWindowProc(h,m,w,l);}
-int WINAPI WinMain(HINSTANCE i,HINSTANCE,LPSTR,int n){
-WNDCLASS wc={0};wc.lpfnWndProc=W;wc.hInstance=i;wc.lpszClassName="X";RegisterClass(&wc);
-HWND h=CreateWindow("X",0,0xCF0000,0,0,640,480,0,0,i,0);ShowWindow(h,n);
-MSG m;while(GetMessage(&m,0,0,0))DispatchMessage(&m);return 0;}
--- p3/makefile.mak
+++
@@ -1,8 +0,0 @@
-.SILENT:
-run:
-	echo message
-case1:
-	mkdir -p bin
-	gcc a.c b.c -o bin/a.out -lX11
-	bin/a.out
-	rm -rf bin
