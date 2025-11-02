8cad35b76fdff213f65aa7ba8b9f0587d29a36f2
takashi <takashi.moriya@gmail.com>
2025-11-02 16:29:19 +0900

    a

--- p5/b8.py
+++ p5/b8.py
@@ -1,16 +1,17 @@
-l1 = list("あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわがぎぐげござじずぜぞばびぶべぼぱぴぷぺぽ")
-def f3(pa):
-    b = 13 * 257
+l = list("あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわがぎぐげござじずぜぞばびぶべぼぱぴぷぺぽ")
+def c(p):
+    b = 3341
     for i in reversed(range(13)):
-        a = int(pa[i], 2)
+        a = int(p[i], 2)
         for _ in range(8):
             b = ((b << 1) ^ 4129 if ((b >> 8) ^ a) & 128 else b << 1) & 0xFFFF
             a = (a << 1) & 0xFF
     return format(b & 2047, "011b")
-a3 = [("0" * 98)[i:i+8].ljust(8, '0') for i in range(0, 98, 8)]
-a4 = f3(a3)
-a3[0] = a4[6:] + a3[0][5:]
-a3[8] = a3[8][:2] + a4[:6]
-a7 = [("".join(a3) + '0' * ((6 - len("".join(a3)) % 6) % 6))[i:i+6] for i in range(0, len("".join(a3)) + ((6 - len("".join(a3)) % 6) % 6), 6)]
-for i in range(1, 18): a7[i] = format((int(a7[i], 2) + int(a7[i-2], 2) + int(a7[0][3:5], 2) + 1) & 63, "06b")
-print(''.join([l1[int(x, 2)] for x in a7]))
+x = [("0"*98)[i:i+8].ljust(8,'0') for i in range(0,98,8)]
+y = c(x)
+x[0] = y[6:] + x[0][5:]
+x[8] = x[8][:2] + y[:6]
+s = "".join(x) + '0'*((6 - len("".join(x)) % 6) % 6)
+z = [s[i:i+6] for i in range(0, len(s), 6)]
+for i in range(1, 18): z[i] = format((int(z[i],2)+int(z[i-2],2)+int(z[0][3:5],2)+1)&63, "06b")
+print(''.join(l[int(i,2)] for i in z))
