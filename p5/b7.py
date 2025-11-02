3d7987dfb617ee38b2f07fc994ba875b8d6e7742
takashi <takashi.moriya@gmail.com>
2025-11-02 15:25:15 +0900

    a

--- p5/b7.py
+++ p5/b7.py
@@ -11,7 +11,6 @@ def f3(pa):
             else:b=(b<<1)&0xFFFF
             a=(a<<1)&0xFF
     return format(b&2047,"11b")
-def f4(pa):return'\n'.join(['  '.join([pa[i:i+3],pa[i+3:i+6],pa[i+6:i+9]])for i in range(0,len(pa),10)])
 a1=[f1(i,l2)for i in"０"*4]
 a3=f2("0"*8+a1[2]+a1[1][:2]+"0"*8+a1[1][3:5]+a1[0]+"0"*8+a1[1][5]+a1[3]+a1[1][2]+"0"*50)
 a4=f3(a3)
@@ -21,4 +20,4 @@ a5=''.join(a3)
 a6=a5+'0'*((6-len(a5)%6)%6)
 a7=[a6[i:i+6]for i in range(0,len(a6),6)]
 for i in range(1,len(a7)):a7[i]=format((int(a7[i],2)+int(a7[i-2],2)+int(a7[0][3:5],2)+1)&63,"6b")
-print(f4(''.join([l1[int(x,2)]for x in a7])))
+print(''.join([l1[int(x,2)]for x in a7]))
