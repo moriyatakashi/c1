e5fa8d7eb1e6fe5c4294b05c3f18425ac6a98793
takashi <takashi.moriya@gmail.com>
2025-11-06 08:09:54 +0900

    a

--- z1/a.py
+++ z1/a.py
@@ -1,13 +1,5 @@
 import tkinter as tk
-
-def on_button1_click():
-    label.config(text="ボタン1がクリックされました！")
-
-def on_button2_click():
-    label.config(text="ボタン2がクリックされました！")
-
-def on_button3_click():
-    label.config(text="ボタン3がクリックされました！")
+import b  # b.py をインポート
 
 # ウィンドウの作成
 root = tk.Tk()
@@ -19,13 +11,13 @@ label = tk.Label(root, text="こんにちは！", font=("Arial", 14))
 label.pack(pady=10)
 
 # ボタンの作成（3つ）
-button1 = tk.Button(root, text="ボタン1", command=on_button1_click)
+button1 = tk.Button(root, text="ボタン1", command=lambda: b.on_button1_click(label))
 button1.pack(pady=5)
 
-button2 = tk.Button(root, text="ボタン2", command=on_button2_click)
+button2 = tk.Button(root, text="ボタン2", command=lambda: b.on_button2_click(label))
 button2.pack(pady=5)
 
-button3 = tk.Button(root, text="ボタン3", command=on_button3_click)
+button3 = tk.Button(root, text="ボタン3", command=lambda: b.on_button3_click(label))
 button3.pack(pady=5)
 
 # メインループの開始
---
+++ z1/b.py
@@ -0,0 +1,8 @@
+def on_button1_click(label):
+    label.config(text="ボタン1がクリックされました！")
+
+def on_button2_click(label):
+    label.config(text="ボタン2がクリックされました！")
+
+def on_button3_click(label):
+    label.config(text="ボタン3がクリックされました！")
