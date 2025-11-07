5bc095771cc1833a169b690abcec107757c0ece6
takashi <takashi.moriya@gmail.com>
2025-11-07 15:42:57 +0900

    a

--- z1/b.py
+++
@@ -1,8 +0,0 @@
-def on_button1_click(label):
-    label.config(text="あああ　あああ　ああああ　あああ　あああ　ああ")
-
-def on_button2_click(label):
-    label.config(text="ボタン2がクリックされました！")
-
-def on_button3_click(label):
-    label.config(text="ボタン3がクリックされました！")
