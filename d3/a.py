30d1e3daa41240b3badf9fe5827dd9d290664e3d
takashi <takashi.moriya@gmail.com>
2025-11-14 19:17:45 +0900

    a

--- d3/a.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-root = tk.Tk()
-root.geometry("640x480")
-menubar = tk.Menu(root)
-file_menu = tk.Menu(menubar, tearoff=0)
-file_menu.add_command(label="quit", command=root.quit)
-menubar.add_cascade(label="file", menu=file_menu)
-root.config(menu=menubar)
-root.mainloop()
--- d3/f1.bat
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/sairoutine/faithjs
-cd faithjs
-npm install webpack-cli@latest webpack-dev-server@latest --save-dev --legacy-peer-deps
-npm run watch
